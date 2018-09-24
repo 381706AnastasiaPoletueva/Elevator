ffa29a1f837a4d6c5f32fc0e5f042a7dfc603343
381706AnastasiaPoletueva <43164380+381706AnastasiaPoletueva@users.noreply.github.com>
2018-09-24 20:57:40 +0300

    Create TPassengers.h

---
+++ TPassengers.h
@@ -0,0 +1,16 @@
+#include "TElevator.h"
+#ifndef __PASSENGERS_H__
+#define __PASSENGERS_H__
+
+class TPassengers:public TElevator 
+{
+protected:
+int NumberOfPeople;
+int price;
+public:
+int GetNumberOfPeople;
+void SetNumberOfPeople;
+int GetPrice;
+void SetPrice;
+};
+#endif
